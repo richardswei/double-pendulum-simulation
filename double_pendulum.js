f885971f3a42966c6a7838c8a71c81349ffa83ae
Richard <wei.richard.s@gmail.com>
2019-08-06 22:15:45 -0700

    add 2 first order equations for the angular acceleration system does not conserve energy...

--- double_pendulum.js
+++ double_pendulum.js
@@ -5,15 +5,16 @@
 	*/
 var simulationOn = false;
 // CONSTANTS
-var mass1 = 10;
-var mass2 = 10;
-var g = 9.8; //gravitational constant
+var m1 = 10; // mass1
+var m2 = 10;	// mass2
+var g = 10; // gravitational constant
+var timeStep = 10;
 
 // POSITIONS
-var r1 = 50;	// length of the pendulum
+var r1 = 100;	// length of the pendulum
 var r2 = 100;	// length of the pendulum
-var theta1 = 0; // initial angular pos (in rads) of the pendulum relative to lower vertical 
-var theta2 = 0;
+var theta1 = Math.PI/4; // initial angular pos (in rads) of the pendulum relative to lower vertical 
+var theta2 = Math.PI/2;
 var x1 = r1*Math.sin(theta1);
 var y1 = r1*Math.cos(theta1);
 var x2 = x1 + r2*Math.sin(theta2);
@@ -26,8 +27,6 @@ var d_theta2 = 0;
 // ACCELERATIONS
 var dd_theta1 = 0;
 var dd_theta2 = 0;
-
-
 
 // DRAW
 var svgContainer = d3.select("#double_pendulum").append("svg")
@@ -69,11 +68,11 @@ var lines = svgContainer.append("path")
 
 // NEXT STEPS
 function step() {
-	// calculate acceleration and velocities
-	d_theta1 += dd_theta1;
-	// circles
   circlesData = circles.data();
+	d_theta1 += getAngularAcc1(g, m1, m2, circlesData[0].theta, circlesData[1].theta, d_theta1, d_theta2);
+	d_theta2 += getAngularAcc2(g, m1, m2, circlesData[0].theta, circlesData[1].theta, d_theta1, d_theta2);
 
+	// calculate acceleration and velocities
   circlesData[0].theta += d_theta1;
   circlesData[0].x_axis = r1*Math.sin(circlesData[0].theta);
   circlesData[0].y_axis = r1*Math.cos(circlesData[0].theta);
@@ -111,7 +110,7 @@ function renderLines(circlesData) {
 
 var startSimulation = setInterval(function(){
 		step();
-	}, 20);
+	}, 1000/timeStep);
 
 // function toggleSimulation() {
 // 	if (simulationOn) {
@@ -122,3 +121,20 @@ var startSimulation = setInterval(function(){
 // 		simulationOn=true;
 // 	}
 // }
+
+function getAngularAcc1(gConst, m1, m2, theta1, theta2, d_theta1, d_theta2) {
+	var term1 = -gConst*(2*m1+m2)*Math.sin(theta1);
+	var term2 = -m2*gConst*Math.sin(theta1-(2*theta2));
+	var term3 = -2*Math.sin(theta1-theta2)*m2*(Math.pow(d_theta2,2)*r2+Math.pow(d_theta1,2)*Math.cos(theta1-theta2));
+	var term4 = r1*(2*m1+m2-m2*Math.cos(2*theta1-2*theta2));
+	console.log((term1+term2+term3)/term4);
+	return (term1+term2+term3)/term4;
+}
+function getAngularAcc2(gConst, m1, m2, theta1, theta2, d_theta1, d_theta2) {
+	var term1 = 2*Math.sin(theta1-theta2);
+	var term2 = Math.pow(d_theta1,2)*r1*(m1+m2);
+	var term3 = gConst*(m1+m2)*Math.cos(theta1);
+	var term4 = Math.pow(d_theta2,2)*r2*m2*Math.cos(theta1-theta2);
+	var term5 = r2*(2*m1+m2-m2*Math.cos(2*theta1-2*theta2));
+	return term1*(term2+term3+term4)/term5;
+}
